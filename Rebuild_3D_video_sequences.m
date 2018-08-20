0b4bbac5d97e78d3870e78bb105867d3441bf50e
JianNuo <twjiannuo@gmail.com>
2018-08-20 14:34:27 -0400

    Finishing basic frame for the algorithm

--- Rebuild_3D_video_sequences.m
+++ Rebuild_3D_video_sequences.m
@@ -18,7 +18,7 @@ env_set()
 % Reserved      14
 % Traffic Light	15
 function env_set()
-    base_path = '/home/ray/ShengjieZhu/Fall Semester/synthia-dataset/SYNTHIA-SEQS-05-SPRING/'; % base file path
+    base_path = '/home/ray/ShengjieZhu/Fall Semester/depth_detection_project/SYNTHIA-SEQS-05-SPRING/'; % base file path
     GT_Depth_path = 'Depth/Stereo_Left/Omni_F/'; % depth file path
     GT_seg_path = 'GT/LABELS/Stereo_Left/Omni_F/'; % Segmentation mark path
     GT_RGB_path = 'RGB/Stereo_Left/Omni_F/';
@@ -29,7 +29,7 @@ function env_set()
     
     n = 958;
     
-    for frame = 1 : n
+    for frame = 1 : 1
         f = num2str(frame, '%06d');
         
         % Get Camera parameter
@@ -43,21 +43,116 @@ function env_set()
         
         % Get segmentation mark groudtruth (Instance id looks broken)
         ImagePath = strcat(base_path, GT_seg_path, f, '.png');
-        [label, ~] = getIDs(ImagePath);
+        [label, instance] = getIDs(ImagePath);
         img = imread(strcat(base_path, GT_RGB_path, num2str((frame-1), '%06d'), '.png'));
         
-        [car_ix, car_iy] = find(label == 8);
-        linear_ind = sub2ind(size(label), car_ix, car_iy);
+        objs = seg_image(depth, label, instance, extrinsic_params, intrinsic_params);
+        objs = get_init_guess(objs);
+        objs = estimate_single_cubic_shape(objs, extrinsic_params, intrinsic_params, 1);
         
-        reconstructed_3d = get_3d_pts(depth, extrinsic_params, intrinsic_params, linear_ind);
-        show_img_on_index(img, 1);
-        figure(2)
-        scatter3(reconstructed_3d(:,1),reconstructed_3d(:,2),reconstructed_3d(:,3),3,'r');
+        draw_scene(objs, 1)
+    end
+    % Check:
+    % mean_error = check_projection(objs, extrinsic_params,
+    % intrinsic_params);
+end
+function extrinsic_params = get_new_extrinsic_params(extrinsic_params)
+    load('affine_matrix.mat');
+    extrinsic_params = extrinsic_params / affine_matrx;
+end
+function objs = estimate_single_cubic_shape(objs, extrinsic_params, intrinsic_params, index)
+    % Iinit:
+    path = '/home/ray/ShengjieZhu/Fall Semester/depth_detection_project/Exp_re/Termination_condition_problem/';
+    extrinsic_params = get_new_extrinsic_params(extrinsic_params);
+    R = extrinsic_params(1:3, 1:3);
+    T = extrinsic_params(1:3, 4);
+    image_size = size(objs{1}.depth_map);
+    camera_origin = (-R' * T)';
+    num1 = 10; num2 = 10;
+    gamma = 0.5;
+    activation_label = [1 1 1 1 1 0];
+    it_count = 0;
+    while true
+        it_count = it_count + 1;
+        cur_activation_label = cancel_co_activation_label(activation_label);
+        
+        cubics = distill_all_eisting_cubic_shapes(objs);
+        cur_pts = sample_cubic_by_num(objs{index}.cur_cuboid, num1, num2);
+        [pts_estimated_2d, pts_estimated_vlaid, ~, depth] = projectPoints(cur_pts, intrinsic_params(1:3, 1:3), extrinsic_params, [0,0,0,0,0], [image_size(1) image_size(2)], false);
+        cur_pts = cur_pts(pts_estimated_vlaid, :); pts_estimated_2d = pts_estimated_2d(pts_estimated_vlaid, :); depth = depth(pts_estimated_vlaid);
+        [visible_pt_3d, visible_pt_2d, visible_depth] = find_visible_pt_global(cubics, pts_estimated_2d, cur_pts, depth, intrinsic_params, extrinsic_params, camera_origin);
+        
+        activated_params_num = sum(double(cur_activation_label));
+        hessian = zeros(activated_params_num, activated_params_num); first_order = zeros(activated_params_num, 1);
+        [hessian, first_order] = analytical_gradient(objs{index}.cur_cuboid, intrinsic_params, extrinsic_params, visible_pt_3d, objs{index}.depth_map, hessian, first_order, cur_activation_label);
+        
+        
+        figure(1)
+        clf
+        scatter3(visible_pt_3d(:,1), visible_pt_3d(:,2), visible_pt_3d(:,3), 3, 'r', 'fill');
+        hold on
+        draw_cuboid(cubics{index});
+        hold on
+        scatter3(objs{index}.new_pts(:,1), objs{index}.new_pts(:,2), objs{index}.new_pts(:,3), 3, 'g', 'fill')
+        % saveas(gcf, [path num2str(it_count) '.png'])
+        [path num2str(it_count) '.png']
+        
+        delta = inv(hessian) * first_order; params_cuboid_order = update_params(objs{index}.guess, delta, gamma, cur_activation_label);
+        objs{index}.guess(1:6) = params_cuboid_order;
+        cx = params_cuboid_order(1); cy = params_cuboid_order(2); theta = params_cuboid_order(3); l = params_cuboid_order(4); w = params_cuboid_order(5); h = params_cuboid_order(6);
+        objs{index}.cur_cuboid = generate_cuboid_by_center(cx, cy, theta, l, w, h);
     end
 end
-function show_img_on_index(image, figure_ind)
-    figure(figure_ind)
-    imshow(image)
+function cubics = distill_all_eisting_cubic_shapes(objs)
+    cubics = cell(length(objs), 1);
+    for i = 1 : length(objs)
+        cubics{i} = objs{i}.cur_cuboid;
+    end
+end
+function objs = get_init_guess(objs)
+    for i = 1 : length(objs)
+        [params, cuboid] = estimate_rectangular(objs{i}.new_pts);
+        objs{i}.guess = params;
+        objs{i}.cur_cuboid = cuboid;
+    end
+end
+function objs = seg_image(depth_map, label, instance, extrinsic_params, intrinsic_params)
+    % Only for car currently;
+    [car_ix, car_iy] = find(label == 8);
+    
+    all_car_pixel = sub2ind(size(label), car_ix, car_iy);
+    to_seg = unique(instance(all_car_pixel));
+    objs = cell(length(to_seg), 1);
+    
+    max_depth = max(max(depth_map));
+    
+    for i = 1 : length(to_seg)
+        objs{i} = struct;
+        objs{i}.instance = to_seg(i);
+        objs{i}.label = 8;
+        
+        [all_cur_instance_ix, all_cur_instance_iy] = find(instance == to_seg(i));
+        all_cur_instance_pixel = sub2ind(size(label), all_cur_instance_ix, all_cur_instance_iy);
+        
+        objs{i}.instance = all_cur_instance_pixel;
+        objs{i}.depth_map = ones(size(depth_map)) * max_depth;
+        objs{i}.depth_map(objs{i}.instance) = depth_map(objs{i}.instance);
+        
+        objs{i}.old_pts = get_3d_pts(depth_map, extrinsic_params, intrinsic_params, objs{i}.instance);
+        objs{i}.new_pts = get_pt_on_new_coordinate_system(objs{i}.old_pts);
+    end
+end
+
+function draw_scene(objs, index)
+    figure(index)
+    clf
+    for i = 1 : length(objs)
+        pts = objs{i}.new_pts;
+        scatter3(pts(:,1), pts(:,2), pts(:,3), 3, 'r', 'fill')
+        hold on
+        draw_cuboid(objs{i}.cur_cuboid)
+    end
+    axis equal
 end
 function reconstructed_3d = get_3d_pts(depth_map, extrinsic_params, intrinsic_params, valuable_ind)
     height = size(depth_map, 1);
@@ -67,4 +162,33 @@ function reconstructed_3d = get_3d_pts(depth_map, extrinsic_params, intrinsic_pa
     pts = [Y(:) X(:)];
     projects_pts = [pts(valuable_ind,2) .* depth_map(valuable_ind), pts(valuable_ind,1) .* depth_map(valuable_ind), depth_map(valuable_ind), ones(length(valuable_ind), 1)];
     reconstructed_3d = (inv(intrinsic_params * extrinsic_params) * projects_pts')';
+end
+function show_depth_map(depth_map)
+    imshow(uint16(depth_map * 1000));
+end
+function show_img_on_index(image, figure_ind)
+    figure(figure_ind)
+    imshow(image)
+end
+function params_cuboid_order = update_params(old_params, delta, gamma, activation_label)
+    activation_label = (activation_label == 1);
+    new_params = old_params;
+    params_derivation_order = [new_params(3), new_params(1), new_params(2), new_params(4), new_params(5), new_params(6)];
+    params_derivation_order(activation_label) = params_derivation_order(activation_label) + gamma * delta';
+    params_cuboid_order = [params_derivation_order(2), params_derivation_order(3), params_derivation_order(1), params_derivation_order(4), params_derivation_order(5), params_derivation_order(6)];
+    if params_cuboid_order(4) < 0 | params_cuboid_order(5) < 0 | params_cuboid_order(6) < 0
+        params_cuboid_order = old_params;
+        disp('Unstable')
+    end
+end
+function activation_label = cancel_co_activation_label(activation_label)
+    activation_label = (activation_label == 1);
+    if (activation_label(2) | activation_label(3)) & (activation_label(5) | activation_label(4))
+        if(randi([1 2], 1) == 1)
+            activation_label(2) = 0; activation_label(3) = 0;
+        else
+            activation_label(5) = 0; activation_label(4) = 0;
+        end
+    end
+
 end
--- inspection_codes.m
+++ inspection_codes.m
@@ -53,3 +53,76 @@ end
 % linear_ind3 = sub2ind([size(label) 3], car_ix, car_iy, ones(length(car_ix), 1) * 3);
 % linear_ind = [linear_ind1;linear_ind2;linear_ind3];
 %}
+%{
+    % Gather all points together
+    num_tot_ground_points = 0;
+    for i = 1 : size(reconstructed_3d, 1)
+        num_tot_ground_points = num_tot_ground_points + size(reconstructed_3d{i}, 1);
+    end
+    tot_ground_points = zeros(num_tot_ground_points, 3);
+    num_tot_ground_points = 0;
+    for i = 1 : size(reconstructed_3d, 1)
+        tot_ground_points(num_tot_ground_points + 1 : num_tot_ground_points + size(reconstructed_3d{i}, 1), :) = reconstructed_3d{i};
+        num_tot_ground_points = num_tot_ground_points + size(reconstructed_3d{i}, 1);
+    end
+%}
+%{
+function affine_transformation = get_affine_transformation(origin, new_basis, param)
+    pt_camera_origin_3d = origin;
+    x_dir = new_basis(1, :);
+    y_dir = new_basis(2, :);
+    z_dir = new_basis(3, :);
+    new_coord1 = [1 0 0];
+    new_coord2 = [0 1 0];
+    new_coord3 = [0 0 1];
+    new_pts = [new_coord1; new_coord2; new_coord3];
+    old_Coord1 = pt_camera_origin_3d + x_dir;
+    old_Coord2 = pt_camera_origin_3d + y_dir;
+    old_Coord3 = pt_camera_origin_3d + z_dir;
+    old_pts = [old_Coord1; old_Coord2; old_Coord3];
+    
+    T_m = new_pts' * inv((old_pts - repmat(pt_camera_origin_3d, [3 1]))');
+    transition_matrix = eye(4,4);
+    transition_matrix(1:3, 1:3) = T_m;
+    transition_matrix(1, 4) = -pt_camera_origin_3d * x_dir';
+    transition_matrix(2, 4) = -pt_camera_origin_3d * y_dir';
+    transition_matrix(3, 4) = -pt_camera_origin_3d * z_dir';
+    affine_transformation = transition_matrix;
+    % Check: 
+    % (affine_transformation * [old_pts ones(3,1)]')'
+end
+%}
+function [dir, origin] = get_affine_transformation_from_plane(param, pts)
+    origin = mean(pts); origin = origin(1:3);
+    dir1 = (rand_sample_pt_on_plane(param) - rand_sample_pt_on_plane(param)); dir1 = dir1 / norm(dir1);
+    dir3 = param(1:3); dir3 = dir3 / norm(dir3);
+    dir2 = cross(dir1, dir3); dir2 = dir2 / norm(dir2);
+    dir =[dir1;dir2;dir3];
+    % dir1_ = [1 0 0];
+    % dir2_ = [0 1 0];
+    % dir3_ = [0 0 1];
+    % dir_ = [dir1_;dir2_;dir3_];
+    % rotation_matrix = dir_' * inv(dir');
+    % affine_transformation = get_affine_transformation(origin, dir, param);
+    % Check:
+    % affine_transformation = dir_' * inv(dir');
+    % affine_transformation(4,4) = 1;
+end
+% Edited on 08/20/18
+% Aimed to inspect Projection
+function mean_error = check_projection(objs, extrinsic_params, intrinsic_params)
+    load('affine_matrix.mat');
+    
+    extrinsic_params = extrinsic_params / affine_matrx;
+    image_size = size(objs{1}.depth_map);
+    
+    sum_error = 0;
+    tot_num = 0;
+    for i = 1 : length(objs)
+        reconstructed_3d_pts = objs{i}.new_pts;
+        [projected, valid, dist, depth] = projectPoints(reconstructed_3d_pts, intrinsic_params(1:3, 1:3), extrinsic_params, [0,0,0,0,0], image_size, true);
+        sum_error = sum_error + sum((depth - objs{i}.depth_map(objs{i}.instance)).^2);
+        tot_num = tot_num + length(objs{i}.instance);
+    end
+    mean_error = sqrt(sum_error / tot_num);
+end
